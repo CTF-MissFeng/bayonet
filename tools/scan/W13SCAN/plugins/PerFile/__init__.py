615b79e37708985c8704e8e37003766b3b025d21
a15803617402 <1767986993@qq.com>
2020-03-04 13:35:33 +0800

    update

--- tools/scan/W13SCAN/plugins/PerFile/__init__.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-# @Time    : 2019/7/4 10:14 PM
-# @Author  : w8ay
-# @File    : __init__.py.py
--- tools/scan/W13SCAN/plugins/__init__.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-# @Time    : 2019/6/29 12:15 AM
-# @Author  : w8ay
-# @File    : __init__.py.py
